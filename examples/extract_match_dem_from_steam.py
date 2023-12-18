234871c38cf220093da2c6f4e9b45ac661d2eae6
Jack Wagner <jack.wagner5299@gmail.com>
2023-12-18 14:54:33 -0600

    #4 finish match code decoder

--- examples/extract_match_dem_from_steam.py
+++ examples/extract_match_dem_from_steam.py
@@ -2,7 +2,6 @@ import requests
 import regex as re
 from json import load,loads
 
-# boiler plate for Share Code decode
 DICTIONARY        = 'ABCDEFGHJKLMNOPQRSTUVWXYZabcdefhijkmnopqrstuvwxyz23456789';
 DICTIONARY_LENGTH = len(DICTIONARY);
 SHARECODE_PATTERN = r'^CSGO(-?[\w]{5}){5}$';
@@ -57,7 +56,7 @@ def get_most_recent_game_code(api_key: str, game_auth_code: str, steam_id: str, 
 
     return most_recent_game
 
-def decode_match_share_code(match_share_code: str):
+def decode_match_share_code(match_share_code: str, verbose = True):
     """
         Returns dictionary containing match_id, reservation_id and tv_port 
         :param match_share_code: match share code to decode
@@ -68,14 +67,47 @@ def decode_match_share_code(match_share_code: str):
 
     clean_share_code = match_share_code.replace('CSGO','').replace('-','')
 
+    reverse_code = list(clean_share_code)
+    reverse_code.reverse()
+
+    code_int = 0
+    for c in reverse_code:
+        code_int = code_int * DICTIONARY_LENGTH + DICTIONARY.index(c)
+
+    code_hex = hex(code_int).lstrip("0x").zfill(36)
+
+    code_bytes = [code_hex[i:i+2] for i in range(0, len(code_hex), 2)] 
+
+    match_id_bytes       = code_bytes[0:8]
+    reservation_id_bytes = code_bytes[8:16]
+    tv_port_bytes        = code_bytes[16:18]
+
+    match_id_bytes.reverse()
+    reservation_id_bytes.reverse()
+    tv_port_bytes.reverse()
+
+    if verbose:
+        print(f"""
+=> {match_share_code}
+=> {clean_share_code} 
+=> {reverse_code}
+=> {code_int} 
+=> {code_hex} 
+=> {code_bytes}
+=> ({match_id_bytes},{reservation_id_bytes},{tv_port_bytes})
+""")
+
     return {
-         "match_id":match_share_code
-        ,"reservation_id":match_share_code
-        ,"tv_port":match_share_code
+         "match_id":int(''.join(match_id_bytes),16)
+        ,"reservation_id":int(''.join(reservation_id_bytes),16)
+        ,"tv_port":int(''.join(tv_port_bytes),16)
     }
 
 
 my_latest = get_most_recent_game_code(api_key, game_auth_code, steam_id, known_game_code)
-
 print(my_latest)
+
+decode_dict = decode_match_share_code(my_latest)
+print(decode_dict)
+
 
