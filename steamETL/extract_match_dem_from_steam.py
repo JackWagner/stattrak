78ccdc658f21d62660f983cf10434e8e109c4a3b
Jack Wagner <jack.wagner5299@gmail.com>
2023-12-20 16:17:44 -0600

    #8 connect to db from local via SQL alchemy

--- steamETL/extract_match_dem_from_steam.py
+++ steamETL/extract_match_dem_from_steam.py
@@ -1,6 +1,38 @@
 from steam.client import SteamClient
 from csgo.client import CSGOClient
+from csgo.sharecode import decode
 import logging
+
+from json import load
+from sqlalchemy import create_engine
+import pandas as pd
+
+db_user_conf = open('examples/config/db_user.json','r')
+db_user = load(db_user_conf)
+
+database = db_user.get('database')
+user     = db_user.get('user')
+host     = db_user.get('host')
+password = db_user.get('password')
+port     = db_user.get('port')
+
+connection_str = f'postgresql://{user}:{password}@{host}:{port}/{database}'
+engine         = create_engine(connection_str)
+connection_res = None
+
+try:
+    
+    connection_res = engine.connect()
+    print(f'Welcome {user}!')
+except Exception as ex:
+    print(f'{ex}')
+    raise
+
+query = "SELECT * FROM users.steam_auth;"
+df = pd.read_sql_query(sql=query,con=connection_res)
+
+print(df)
+print(decode(df.iloc[0]['known_game_code']))
 
 logging.basicConfig(format='[%(asctime)s] %(levelname)s %(name)s: %(message)s', level=logging.DEBUG)
 
@@ -18,3 +50,4 @@ def gc_ready():
 
 client.cli_login()
 client.run_forever()
+
