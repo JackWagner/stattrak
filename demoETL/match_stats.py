79b26f1f602c2d843b5a6fbf0c0c9f1899edafeb
Jack Wagner <jack.wagner5299@gmail.com>
2024-01-05 13:28:55 -0600

    #26 added match stats json layout second passs

--- demoETL/match_stats.py
+++ demoETL/match_stats.py
@@ -1,35 +1,23 @@
 """ match stat example layout
-{
-     'flashbangStats':{
-        [{
-             "user_name":"IEM24_Applicant"
-            ,"user_team":True
-            ,"count_team_flash":5
-            ,"sum_blind_duration":7.12342
-        },{
-             "user_name":"LilRev"
-            ,"user_team":False
-            ,"count_team_flash":2
-            ,"sum_blind_duration":1.32423
-        },
-        ...
-        ]
+[
+    {
+         "user_name":"IEM24_Applicant"
+        ,"user_team":True
+        ,'stats':{
+                ,"count_team_flash":5
+                ,"sum_blind_duration":7.12342
+                ,"sum_team_dmg":55
+                }
+    },{
+         "user_name":"Agent Smith"
+        ,"user_team":False
+        ,'stats':{
+                ,"count_team_flash":18
+                ,"sum_blind_duration":23.043745
+                ,"sum_team_dmg":234
+                }
     }
-    ,'utilDamageStats':{
-        [{
-             "user_name":"IEM24_Applicant"
-            ,"user_team":True
-            ,"sum_team_dmg":35
-        },{
-             "user_name":"LilRev"
-            ,"user_team":False
-            ,"sum_team_dmg":55
-        },
-        ...
-        ]
-    }
-    ...
-}
+]
 """
 def get_outlier_match_stat(match_stats,user_team_only:bool = True):
     for stat_type,match_stat in match_stats.items():
