27f6f2cfed990bc092ece1ff3ac699c3536a2a37
vldt-jww <jack.wagner@validatehealth.com>
2024-01-07 20:22:21 -0600

    #26 add demo parsing, demo stat utilities and automated outlier detection to match_stats file

--- demoETL/match_stats.py
+++ demoETL/match_stats.py
@@ -1,5 +1,43 @@
 from math import sqrt
 
+# Demo Parsing 
+
+def get_team_flashed_by_player(demoparser):
+    # Get team number by player
+    team_df = demoparser.parse_event("player_team")[['team','user_steamid']].set_index('user_steamid')
+
+    # get all flashbang events and filter out warmup
+    all_flashed_events_df = demoparser.parse_event("player_blind", other=["is_warmup_period"])
+    flashed_events_df     = all_flashed_events_df[all_flashed_events_df["is_warmup_period"] == False]
+
+    # Join in the flashee team and flasher team sequentially
+    flashes_w_user_team_df = flashed_events_df.merge(team_df, on='user_steamid')
+    flashes_w_both_team_df = flashes_w_user_team_df.merge(team_df, left_on='attacker_steamid', right_on='user_steamid', suffixes=['_user','_attacker'])
+
+    # Filter to where flashee team and flasher team are the same
+    team_flashes_df = flashes_w_both_team_df[flashes_w_both_team_df["team_user"]==flashes_w_both_team_df["team_attacker"]]
+
+    #print(team_flashes_df)
+
+    # Aggregate and sort by team
+    team_flash_leaderboard_df = team_flashes_df[['attacker_name','team_attacker','blind_duration']].groupby(['attacker_name','team_attacker']).agg(['count','sum'])
+    team_flash_leaderboard_df.rename(columns={'attacker_name': 'user_name'}, inplace=True)
+    #print(team_flash_leaderboard_df)
+    print(team_flash_leaderboard_df.sort_values(by=[('blind_duration','count')], ascending=False))
+    return team_flash_leaderboard_df
+
+# Demo Stats Utils
+
+def get_merged_stat_df(stat_df_list:list, key:str='user_name'):
+    if len(stat_df_list) == 1:
+        return stat_df_list[0]
+
+    stat_dfs_w_index = [stat_df.set_index(key) for stat_df in stat_df_list]
+    return stat_dfs_w_index[0].join(stat_dfs_w_index[1:])
+
+def get_stat_json(df):
+    return df.to_json(orient="records")
+
 """
 match_stats = [
     {
@@ -50,6 +88,8 @@ match_stats = [
     }
 ]
 """
+
+# Automated Outlier Detection
 
 def get_stat_types_list(match_stats:list):
     stat_types = []
