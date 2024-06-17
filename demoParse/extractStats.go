fb0c68b4dd8e69265f39c2637260f0124418a5a7
vldt-jww <jack.wagner@validatehealth.com>
2024-06-17 09:13:33 -0500

    Extract player flashbang stats into dataframe

--- demoParse/extractStats.go
+++ demoParse/extractStats.go
@@ -5,9 +5,19 @@ import (
 	"log"
 	"os"
 
+	dataframe "github.com/go-gota/gota/dataframe"
+	series "github.com/go-gota/gota/series"
 	dem "github.com/markus-wa/demoinfocs-golang/v4/pkg/demoinfocs"
 	events "github.com/markus-wa/demoinfocs-golang/v4/pkg/demoinfocs/events"
 )
+
+//TeamCounterTerrorists() *common.TeamState
+// TeamTerrorists returns the TeamState of the T team.
+//
+// Make sure to handle swapping sides properly if you keep the reference.
+//TeamTerrorists() *common.TeamState
+// Participants returns a struct with all currently connected players & spectators and utility functions.
+// The struct contains references to the original maps so it's always up-to-date.
 
 func main() {
 	f, err := os.Open("/Users/vldt-jww/demos/example.dem")
@@ -19,9 +29,16 @@ func main() {
 	p := dem.NewParser(f)
 	defer p.Close()
 
+	Players := series.New([]string{}, series.String, "Players")
+	Attackers := series.New([]string{}, series.String, "Attackers")
+	FlashDurations := series.New([]float64{}, series.Float, "Flash Durations")
+
 	// Register handler on flashbang events and print summary string
 	p.RegisterEventHandler(func(e events.PlayerFlashed) {
-		fmt.Printf("%s flashed %s for %s\n",e.Attacker,e.Player,e.FlashDuration())
+		Players.Append([]string{e.Player.Name})
+		Attackers.Append([]string{e.Attacker.Name})
+		FlashDurations.Append([]float64{float64(e.FlashDuration())})
+		fmt.Printf("%s flashed %s for %s\n", e.Attacker, e.Player, e.FlashDuration())
 	})
 
 	// Parse to end
@@ -29,4 +46,17 @@ func main() {
 	if err != nil {
 		log.Panic("failed to parse demo: ", err)
 	}
+
+	// Create a dataframe from the data
+	df := dataframe.New(
+		Players,
+		Attackers,
+		FlashDurations,
+	)
+
+	// Print dataframe
+	fmt.Println(df)
+
+	// Summary statistics
+	fmt.Println(df.Describe())
 }
