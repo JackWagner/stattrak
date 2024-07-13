2013ba5d5b06682f7615b9c10ee0ce5440c78b34
vldt-jww <jack.wagner@validatehealth.com>
2024-07-13 11:17:19 -0500

    #0027 new parse by frame implementation

--- demoParse/extractStats.go
+++ demoParse/extractStats.go
@@ -30,6 +30,7 @@ func getPlayers(p dem.Parser) []*common.Player {
 			//p.Close()
 		}
 	})
+
 	return all_players
 }
 
@@ -70,6 +71,7 @@ func getKills(p dem.Parser) dataframe.DataFrame {
 		victim.Append([]string{e.Victim.Name})
 		killer.Append([]string{e.Killer.Name})
 		time.Append([]int64{p.CurrentTime().Milliseconds()})
+		fmt.Printf("%s killed %s at %d\n", e.Victim.Name, e.Killer.Name, p.CurrentTime().Milliseconds())
 	})
 	return dataframe.New(victim, killer, time)
 }
@@ -86,28 +88,68 @@ func getInjuries(p dem.Parser) dataframe.DataFrame {
 		healthDamage.Append([]int64{int64(e.HealthDamageTaken)})
 		armorDamage.Append([]int64{int64(e.ArmorDamageTaken)})
 		time.Append([]int64{p.CurrentTime().Milliseconds()})
+		fmt.Printf("%s damaged %s at %d\n", e.Attacker.Name, e.Player.Name, p.CurrentTime().Milliseconds())
 	})
 	return dataframe.New(victim, attacker, healthDamage, armorDamage, time)
 }
 
+func setInjuriesEventHandler(p dem.Parser) {
+	p.RegisterEventHandler(func(e events.PlayerHurt) {
+		fmt.Printf("%s damaged %s at %d\n", e.Attacker.Name, e.Player.Name, p.CurrentFrame())
+	})
+}
+
 func main() {
-	f, err := os.Open("/Users/vldt-jww/demos/example.dem")
-	if err != nil {
-		log.Panic("failed to open demo file: ", err)
+	f, open_err := os.Open("/Users/vldt-jww/demos/example.dem")
+	if open_err != nil {
+		log.Panic("failed to open demo file: ", open_err)
 	}
 	defer f.Close()
 
 	p := dem.NewParser(f)
 	defer p.Close()
 
-	players := getPlayers(p)
+	p.RegisterEventHandler(func(e events.PlayerHurt) {
+		fmt.Printf("%s damaged %s at %d\n", e.Attacker.Name, e.Player.Name, p.CurrentFrame())
+	})
+
+	for {
+		// Parse the next frame
+		moreFrames, err := p.ParseNextFrame()
+		if err != nil {
+			fmt.Printf("Error reached on Frame: %d\n", p.CurrentFrame())
+			log.Panic("failed to parse demo: ", err)
+			break
+		}
+		if !moreFrames {
+			fmt.Printf("End of demo reached on Frame: %d\n", p.CurrentFrame())
+			break
+		}
+
+		// Do something with the frame
+		//fmt.Printf("Parsed Frame: %d\n", p.CurrentFrame())
+	}
+
+	/*players := getPlayers(p)
 	fmt.Printf("Number of Players = %d\n", len(players))
 
-	df := getTeamFlashes(p)
+	/*flash_df := getTeamFlashes(p)*/
+	//damage_df := getInjuries(p)
+	/*kill_df := getKills(p)*/
 
 	// Print dataframe
-	fmt.Println(df)
-
+	/*fmt.Println(flash_df)
 	// Summary statistics
-	fmt.Println(df.Describe())
+	fmt.Println(flash_df.Describe())*/
+
+	// Print dataframe
+	//fmt.Println(damage_df)
+	// Summary statistics
+	//fmt.Println(damage_df.Describe())
+
+	// Print dataframe
+	/*fmt.Println(flash_df)
+	// Summary statistics
+	fmt.Println(flash_df.Describe())*/
+
 }
