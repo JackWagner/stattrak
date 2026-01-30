02c26f5019fa57468a397bc767be9c0c9b94bd58
Jack Wagner <jack.wagner5299@gmail.com>
2026-01-30 16:26:26 -0600

    revert to main branch

--- demoETL/demo.py
+++ demoETL/demo.py
@@ -9,28 +9,6 @@ from demoparser2 import DemoParser
 URL_REGEX  = r'^http://replay115.valve.net/730/\w{32}.dem.bz2'
 FILE_REGEX = r'^\w{32}.dem.bz2$'
 
-def get_team_flashed_by_player(demoparser):
-    # Get team number by player
-    team_df = demoparser.parse_event("player_team")[['team','user_steamid']].set_index('user_steamid')
-
-    # get all flashbang events and filter out warmup
-    all_flashed_events_df = demoparser.parse_event("player_blind", other=["is_warmup_period"])
-    flashed_events_df     = all_flashed_events_df[all_flashed_events_df["is_warmup_period"] == False]
-
-    # Join in the flashee team and flasher team sequentially
-    flashes_w_user_team_df = flashed_events_df.merge(team_df, on='user_steamid')
-    flashes_w_both_team_df = flashes_w_user_team_df.merge(team_df, left_on='attacker_steamid', right_on='user_steamid', suffixes=['_user','_attacker'])
-
-    # Filter to where flashee team and flasher team are the same
-    team_flashes_df = flashes_w_both_team_df[flashes_w_both_team_df["team_user"]==flashes_w_both_team_df["team_attacker"]]
-
-    #print(team_flashes_df)
-
-    # Aggregate and sort by team
-    team_flash_leaderboard_df = team_flashes_df[['attacker_name','team_attacker','blind_duration']].groupby(['attacker_name','team_attacker']).agg(['count','sum'])
-    #print(team_flash_leaderboard_df)
-    print(team_flash_leaderboard_df.sort_values(by=[('blind_duration','count')], ascending=False))
-    return team_flash_leaderboard_df
 
 class Demo():
     def __init__(self, url:str):
